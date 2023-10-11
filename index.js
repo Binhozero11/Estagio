b03d06438468cecb7132c909bf41dd070b94e8fd
Binhozero11 <123664808+Binhozero11@users.noreply.github.com>
2023-10-11 15:15:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const menuButton = document.querySelector("nav i")
 const divLinks = document.querySelector("div.nav-links")
-const titulo = document.querySelector("div.layout-titulo")
+const pagina = document.querySelector("main.cont-js")
+const nav = document.querySelector("nav.nav")
+function isViewTablet() {
+    return window.innerWidth >= 720
+}
 menuButton.addEventListener("click", () => {
     menuButton.classList.toggle("botao-js")
     divLinks.classList.toggle("nav-menu")
+    nav.classList.toggle("nav-js")
     let verificarClasse = divLinks.classList.contains("nav-menu")
-    console.log(verificarClasse);
     if (verificarClasse === true) {
-        titulo.addEventListener("click", () => {
+        pagina.addEventListener("click", () => {
+            menuButton.classList.remove("botao-js")
             divLinks.classList.remove("nav-menu")
+            nav.classList.remove("nav-js")
         })
-        
     }
 })
-
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('resize', () => {
+        let tamanhoDaTela = isViewTablet();
+        if (tamanhoDaTela === true) {
+            menuButton.classList.remove("botao-js")
+            divLinks.classList.remove("nav-menu")
+            nav.classList.remove("nav-js")
+        }
+    })
+})
