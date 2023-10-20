624da55724bbd2ace092c17217162950fe6ff653
Binhozero11 <123664808+Binhozero11@users.noreply.github.com>
2023-10-20 10:25:12 -0300

    Add files via upload

--- assests/js/municipios.js
+++ assests/js/municipios.js
@@ -6,27 +6,24 @@ const header = document.getElementById("header")
 const htmlDoHeader = 
 `
     <header class="layout-cabecalho">
-    <h2>3°INFORMÁTICA</h2>
-    <nav class="nav">
-    <i class="bi bi-brightness-high" id="fundoSimples"></i>
-    <i class="bi bi-brightness-high-fill" id="fundoDoMunicipio"></i>
-    <i class='bx bx-menu' id="menu"></i>
-    <div class="nav-links">
-        <a href="#1">QUEM SOMOS?<i class="bi bi-caret-down"></i></a>
-        <a href="#2">FALE CONOSCO<i class="bi bi-caret-down"></i></a>
-        <a href="#3">DISCIPLINAS<i class="bi bi-caret-down"></i></a>
-    </div>
-    </nav>
+        <a href="../index.html">
+            <h2>3°INFORMÁTICA</h2>
+        </a>
+        <nav class="nav">
+            <i class="bi bi-brightness-high" id="fundo"></i>
+        </nav>
     </header>
 
 `
+header.innerHTML = htmlDoHeader
+
 /*-----------------------*\
     botao trocar tema
 /*-----------------------*/
 
 
 
-header.innerHTML = htmlDoHeader
+
 
 /*-----------------------*\
     footer dos municipios
